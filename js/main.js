105f1df16e2c4be93159c66dd8adc278dc6b91fe
Ashitha-balla <98507541+Ashitha-balla@users.noreply.github.com>
2022-01-27 11:53:50 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// $('.accordionBody').hide();
+// $('#accordionBodyOne').show();
+// $('#accordionHeadOne').click(function(e){
+//     $('.accordionBody').slideUp();
+//     $('#accordionBodyOne').slideDown()
+// })
+// $('#accordionHeadTwo').click(function(){
+//     $('.accordionBody').slideUp();
+//     $('#accordionBodyTwo').slideDown();
+    
+// })
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
